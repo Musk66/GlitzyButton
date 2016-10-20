e789c2d93b6b42afe6bfc44bb43a6434470c839f
tiger <tiger@tiger.local>
2016-10-20 12:33:40 +0800

    Initial Commit

---
+++ GlitzyButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GlitzyButton
+//
+//  Created by tiger on 16/10/20.
+//  Copyright © 2016年 tiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GlitzyButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GlitzyButton
+//
+//  Created by tiger on 16/10/20.
+//  Copyright © 2016年 tiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
